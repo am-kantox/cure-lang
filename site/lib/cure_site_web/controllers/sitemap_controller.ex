9143e00ae97d76aa537f4d2a9cfed63ee117432d
Aleksei Matiushkin <am@ambment.cat>
2026-04-26 09:31:26 +0200

    :books: `sitemap.xml`

--- site/lib/cure_site_web/controllers/sitemap_controller.ex
+++ site/lib/cure_site_web/controllers/sitemap_controller.ex
@@ -158,7 +158,7 @@ defmodule CureSiteWeb.SitemapController do
 
     """
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     #{body}
     </urlset>
     """
