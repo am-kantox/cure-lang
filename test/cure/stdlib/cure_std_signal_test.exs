82ebfec7a55ea4c1106586d63df59d9838e02eee
Made In Heaven <noreply@email.com>
2026-06-27 00:46:22 +1000

    feat(std-signal): map, map_to, to_unit (snake_case naming)

--- test/cure/stdlib/cure_std_signal_test.exs
+++ test/cure/stdlib/cure_std_signal_test.exs
@@ -20,4 +20,24 @@ defmodule Cure.Stdlib.SignalTest do
       assert @sig.never() == {:sig, {:none}}
     end
   end
+
+  describe "map / mapTo / toUnit" do
+    test "map applies f to a present value" do
+      assert @sig.map(fn x -> x * 2 end, {:sig, {:some, 5}}) == {:sig, {:some, 10}}
+    end
+
+    test "map preserves absence" do
+      assert @sig.map(fn x -> x * 2 end, {:sig, {:none}}) == {:sig, {:none}}
+    end
+
+    test "map_to replaces a present value with a constant" do
+      assert @sig.map_to(:hit, {:sig, {:some, 99}}) == {:sig, {:some, :hit}}
+      assert @sig.map_to(:hit, {:sig, {:none}}) == {:sig, {:none}}
+    end
+
+    test "to_unit maps a present value to the unit value nil" do
+      assert @sig.to_unit({:sig, {:some, 7}}) == {:sig, {:some, nil}}
+      assert @sig.to_unit({:sig, {:none}}) == {:sig, {:none}}
+    end
+  end
 end
